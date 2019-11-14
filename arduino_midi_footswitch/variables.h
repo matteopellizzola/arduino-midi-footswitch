d317404b5719ed2494e360458adb604bb2d1dd3e
Simone <simone.denada@gmail.com>
2019-11-14 20:51:34 +0100

    WIP

--- arduino_midi_footswitch/variables.h
+++ arduino_midi_footswitch/variables.h
@@ -4,45 +4,30 @@ const byte button_pins[5] = {2, 3, 4, 5, 6};   // Pins where buttons are connect
 const byte switch_pins[2] = {8, 9};            // Pins where the switch is connected
 const byte led_pins[5] = {10, 16, 14, 15, 18}; // Pins where LEDs are connected
 
-// Layer: this is the currently select layer (changed by the switch)
+// Layer: this is the current selected layer (changed by the switch)
 byte current_layer; // 0 = switch up, 1 = switch off, 2 = switch down
 
-// CC values are the numbers in the decimal column of this table
-// https://www.midi.org/specifications-old/item/table-3-control-change-messages-data-bytes-2
-
+/*
+Layer button order
+,-----------------,
+|  1     |     2  |
+|-----------------|
+|  3  |  4  |  5  |
+`-----------------`
+CC values are the numbers in the decimal column of this table
+https://www.midi.org/specifications-old/item/table-3-control-change-messages-data-bytes-2
+*/
 const int button_layers[2][5] = {
-    /*
-    Layer 0 (Switch Up)
-    ,-----------------,
-    |  49    |     50 |
-    |-----------------|
-    |  51 |  80 |  81 |
-    `-----------------`
-    */
-    {
-        49,     50,
-        51, 80, 81},
-
-    /*
-    Layer 1 (Switch Off)
-    ,-----------------,
-    |  16    |     17 |
-    |-----------------|
-    |  18 |  19 |  48 |
-    `-----------------`
-    */
-    {
-        16,     17,
-        18, 19, 48}
+    {49, 50, 51, 80, 81}, // Layer 0 (Switch Up)
+    {16, 17, 18, 19, 48} // Layer 1 (Switch Off)
 };
 
-// Button modes: 0 = push button, 1 = toggle button
+// Button modes: 
+// 0 = Push button
+// 1 = Switch button
 byte button_modes[2][5] = {
-    // Layer 0 (Switch Up)
-    {0, 0, 0, 0, 0},
-
-    // Layer 1 (Switch Off)
-    {0, 0, 0, 0, 0}
+    {0, 0, 0, 0, 0}, // Layer 0 (Switch Up)
+    {0, 0, 0, 0, 0} // Layer 1 (Switch Off)
 };
 
 int buttonState = 0; 
